7c552926a3e66e3a92a5d579447fc143235cdbca
Andrew Livingstone, Jr. <andrewlivingstonejr@gmail.com>
2024-11-12 21:12:16 +0000

    coding..

---
+++ ncyab-site/src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div>
+      <h1>Welcome to our Page</h1>
+      <p>
+        <ul>
+          <li><a>Listing Items</a></li>
+          <li><a>Listing Items</a></li>
+          <li><a>Listing Items</a></li>
+          <li><a>Listing Items</a></li>
+        </ul>
+      </p>
+    </div>
+  )
+}
